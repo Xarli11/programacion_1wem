bb152c2f09dd84781b6dd54b9ba42ffcbab72d06
Xarlii11 <carlosgt2001@icloud.com>
2022-02-14 22:56:16 +0100

    Añadidos ejemplos de la Clase numero 9

---
+++ trimestre2/Clase9-Ejemplo1.java
@@ -0,0 +1,25 @@
+//Ejemplos de la Clase 9
+
+public class  Clase9 {
+
+    // declare instance variables
+    type var1;
+    type var2;
+// ...
+ //   type varN;
+// declaracion de los metodos
+
+    type method1(parameters) {
+// cuerpo del metodo
+    }
+
+    type method2(parameters) {
+// cuerpo del metodo
+    }
+// ...
+
+    type methodN(parameters) {
+// cuerpo del metodo
+    }
+
+}
---
+++ trimestre2/Clase9-Ejemplo2.java
@@ -0,0 +1,11 @@
+//Ejemplos de la Clase 9
+
+public class  Clase9 {
+
+    class Vehicle {
+        int passengers; // nombre de pasajeros
+        int fuelcap; // capacidad de gasolina
+        int mpg; // condsumo de gasolina por milla
+    }
+    
+}
